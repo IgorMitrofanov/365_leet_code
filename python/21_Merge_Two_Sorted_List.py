446d72a05d149194fd2c085bcd90552721223bb3
IgorMitrofanov <i.v.mitrofanov@ya.ru>
2025-12-10 11:14:24 +0300

    from day two

--- python/21_Merge_Two_Sorted_List.py
+++ python/21_Merge_Two_Sorted_List.py
@@ -32,17 +32,38 @@ Both list1 and list2 are sorted in non-decreasing order.
 """
 
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Solution(object):
-    def mergeTwoLists(self, list1, list2):
+    def mergeTwoLists(self, list1: ListNode, list2: ListNode):
         """
         :type list1: Optional[ListNode]
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+        
+        dummy = ListNode()
+        tail = dummy
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+        if list1:
+            tail.next = list1
+        else:
+            tail.next = list2
+        return dummy.next
+
         
         

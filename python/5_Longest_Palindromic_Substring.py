818cf4b1ab3a4681a01dd78189377f1c22d074cf
IgorMitrofanov <i.v.mitrofanov@ya.ru>
2025-12-16 18:48:35 +0300

    9

---
+++ python/5_Longest_Palindromic_Substring.py
@@ -0,0 +1,52 @@
+"""
+Given a string s, return the longest palindromic substring in s.
+
+ 
+
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+
+Example 2:
+
+Input: s = "cbbd"
+Output: "bb"
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s consist of only digits and English letters.
+"""
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) < 2:
+            return s
+        
+        start, max_length = 0, 1
+        
+        for i in range(len(s)):
+            # Odd length palindromes
+            left, right = i, i
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                current_length = right - left + 1
+                if current_length > max_length:
+                    start = left
+                    max_length = current_length
+                left -= 1
+                right += 1
+            
+            # Even length palindromes
+            left, right = i, i + 1
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                current_length = right - left + 1
+                if current_length > max_length:
+                    start = left
+                    max_length = current_length
+                left -= 1
+                right += 1
+        
+        return s[start:start + max_length]

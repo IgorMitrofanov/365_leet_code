2ff26c3508ddeeb6fdfa8ca64f2f8c70c8e393af
IgorMitrofanov <i.v.mitrofanov@ya.ru>
2026-01-16 17:54:51 +0300

    fix

--- python/704_Binary Search.py
+++ python/704_Binary Search.py
@@ -1,29 +1,26 @@
 from typing import List
 
 class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
+    def search(self, nums: List[int], target: int) -> int:
         left = 0
         right = len(nums) - 1
+        found_idx = -1
         while left <= right:
             mid = (left + right) // 2
             if nums[mid] == target:
-                return mid
-            
-            if left == right:
+                found_idx = mid
                 break
-            
             elif nums[mid] < target:
                 # right side
                 left = mid + 1
             else:
                 # left side
                 right = mid - 1 
-        return mid if nums[mid] > target else mid + 1
+        return found_idx
 
-
-nums = [1,3]
-target = 2
+nums = [-1, 0, 3, 5, 9, 12]
+target = 9
 
 sol = Solution()
 
-print(sol.searchInsert(nums=nums, target=target))
+print(sol.search(nums=nums, target=target))
